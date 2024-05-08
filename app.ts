c9504d71a45702177f3df8c62384677c1a19e4fa
Abid Dar <abiddar1979@gmail.com>
2024-05-08 09:21:37 +0500

    files added

---
+++ app.ts
@@ -0,0 +1,11 @@
+/*Task no 19# Dinner Guests: Working with one of the programs from exercise 14
+through 18 , print a massage indicating the number of the people you are inviting to
+Dinner.*/
+
+let Guest_List : string [] = ["Abid","Asim","Hassan","Saira"];
+
+// for (let i=0; i < Guest_list.length; i++ ){
+ // console.log(`Dear ${Guest_List[i]} : \n you are invited to dinner!\n`);
+// }
+
+console.log(`Total no of people invited : ${Guest_List.length}`);
